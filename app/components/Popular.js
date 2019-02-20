e9f8418258b8a960bef522c925b2f458e24a196b
Matt Campbell <matthewricecampbell@gmail.com>
2019-02-20 17:30:04 -0500

    Add Loading component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var PropTypes = require('prop-types');
 var api = require('../utils/api');
+var Loading = require('./Loading');
 
 //Stateless Functional Component (receives everything as props - NO STATE ;-))
 function SelectLanguage (props){
@@ -96,7 +97,7 @@ class Popular extends React.Component {
           onSelect={this.updateLanguage}
         />
       {!this.state.repos
-        ? <p>LOADING</p>
+        ? <Loading />
         : <RepoGrid repos={this.state.repos} />}
       </div>
     )
