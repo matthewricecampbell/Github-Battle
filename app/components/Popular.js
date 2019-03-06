5f48ffc4a97c2b624f48b09e2490bd3998c72ba8
Matt Campbell <matthewricecampbell@gmail.com>
2019-03-06 11:32:21 -0500

    Refactor to use Class Props

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -53,25 +53,20 @@ SelectLanguage.propTypes = {
 };
 
 class Popular extends React.Component {
-  constructor(props) {
-    super();
-    this.state = {
-      selectedLanguage: 'All',
-      repos: null,
-    };
-
-    this.updateLanguage = this.updateLanguage.bind(this);
+  state = {
+    selectedLanguage: 'All',
+    repos: null,
   }
   componentDidMount() {
     this.updateLanguage(this.state.selectedLanguage)
   }
-  updateLanguage(lang) {
+  updateLanguage = (lang) => {
     this.setState(() => ({
       selectedLanguage: lang,
       repos: null
     }));
 
-		fetchPopularRepos(lang)
+    fetchPopularRepos(lang)
       .then((repos) => this.setState(() => ({ repos })));
   }
   render() {
