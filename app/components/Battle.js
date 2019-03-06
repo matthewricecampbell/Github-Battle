8e7c3a352c42bd05476e7e35698824f9663f5f72
Matt Campbell <matthewricecampbell@gmail.com>
2019-03-06 10:34:57 -0500

    Convert Battle to ES6

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,7 +1,7 @@
-var React = require('react');
-var PropTypes = require('prop-types');
-var Link = require('react-router-dom').Link;
-var PlayerPreview = require('./PlayerPreview');
+const React = require('react');
+const PropTypes = require('prop-types');
+const Link = require('react-router-dom').Link;
+const PlayerPreview = require('./PlayerPreview');
 
 class PlayerInput extends React.Component {
   constructor(props) {
@@ -14,13 +14,9 @@ class PlayerInput extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
   handleChange(event) {
-    var value = event.target.value;
+    const value = event.target.value;
 
-    this.setState(function () {
-      return {
-        username: value
-      }
-    });
+    this.setState(() => ({ username: value }))
   }
   handleSubmit(event) {
     event.preventDefault();
@@ -31,21 +27,24 @@ class PlayerInput extends React.Component {
     );
   }
   render() {
+    const { username } = this.state
+    const { label } = this.props
+
     return (
       <form className='column' onSubmit={this.handleSubmit}>
-        <label className='header' htmlFor='username'>{this.props.label}</label>
+        <label className='header' htmlFor='username'>{label}</label>
         <input
           id='username'
           placeholder='github username'
           type='text'
-          value={this.state.username}
+          value={username}
           autoComplete='off'
           onChange={this.handleChange}
         />
         <button
           className='button'
           type='submit'
-          disabled={!this.state.username}>
+          disabled={!username}>
             Submit
         </button>
       </form>
@@ -74,34 +73,22 @@ class Battle extends React.Component {
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleReset = this.handleReset.bind(this);
   }
   handleSubmit(id, username) {
-    this.setState(function () {
-      var newState = {};
-      newState[id + 'Name'] = username;
-      newState[id + 'Image'] = 'https://github.com/' + username + '.png?size=200'
-      return newState;
-    });
+    this.setState(() => ({
+      [id + 'Name']: username,
+      [id + 'Image']: `https://github.com/${username}.png?size=200`
+    }))
   }
-
-  
   handleReset(id) {
-    this.setState(function () {
-      var newState = {};
-      newState[id + 'Name'] = '';
-      newState[id + 'Image'] = null;
-      return newState;
-    });
+    this.setState(() => ({
+      [id + 'Name']: '',
+      [id + 'Image']: null
+    }))
   }
   render() {
-    var match = this.props.match
-    var playerOneName = this.state.playerOneName;
-    var playerOneImage = this.state.playerOneImage;
-    var playerTwoName = this.state.playerTwoName;
-    var playerTwoImage = this.state.playerTwoImage;
-
-  
+    const { match } = this.props;
+    const { playerOneName, playerOneImage, playerTwoName, playerTwoImage } = this.state;
 
     return (
       <div>
@@ -113,16 +100,16 @@ class Battle extends React.Component {
               onSubmit={this.handleSubmit}
             />}
 
-            {playerOneImage !== null &&
-                <PlayerPreview
-                  avatar={playerOneImage}
-                  username={playerOneName}>
-                    <button
-                      className='reset'
-                      onClick={this.handleReset.bind(this, 'playerOne')}>
-                        Reset
-                    </button>
-                </PlayerPreview>}
+          {playerOneImage !== null &&
+            <PlayerPreview
+              avatar={playerOneImage}
+              username={playerOneName}>
+                <button
+                  className='reset'
+                  onClick={() => this.handleReset('playerOne')}>
+                    Reset
+                </button>
+            </PlayerPreview>}
 
           {!playerTwoName &&
             <PlayerInput
@@ -131,23 +118,24 @@ class Battle extends React.Component {
               onSubmit={this.handleSubmit}
             />}
 
-            {playerTwoImage !== null &&
-              <PlayerPreview
-                avatar={playerTwoImage}
-                username={playerTwoName}>
-                  <button
-                    className='reset'
-                    onClick={this.handleReset.bind(this, 'playerTwo')}>
-                      Reset
-                  </button>
-              </PlayerPreview>}
+          {playerTwoImage !== null &&
+            <PlayerPreview
+              avatar={playerTwoImage}
+              username={playerTwoName}>
+                <button
+                  className='reset'
+                  onClick={() => this.handleReset('playerTwo')}>
+                    Reset
+                </button>
+            </PlayerPreview>}
         </div>
+
         {playerOneImage && playerTwoImage &&
           <Link
             className='button'
             to={{
               pathname: match.url + '/results',
-              search: '?playerOneName=' + playerOneName + '&playerTwoName=' + playerTwoName
+              search: `?playerOneName=${playerOneName}&playerTwoName=${playerTwoName}`
             }}>
               Battle
           </Link>}
